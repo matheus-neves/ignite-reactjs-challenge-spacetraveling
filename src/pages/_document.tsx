275a4a359137dfefa58419a641dea38a29d3c0c4
Matheus Neves <matheusneves1989@gmail.com>
2021-04-21 08:39:31 -0300

    feat: Added document tags, font, favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,23 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <title>spacetraveling</title>
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
