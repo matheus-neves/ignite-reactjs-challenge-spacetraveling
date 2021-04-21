22256c5f8a9ea0901c1e7ec41883c46334676bce
Matheus Neves <matheusneves1989@gmail.com>
2021-04-21 16:14:36 -0300

    feat: Consuming API and listing posts + pagination.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -25,53 +30,96 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const { next_page, results } = postsPagination;
+
+  const [nextPage, setNextPage] = useState(next_page);
+  const [posts, setPosts] = useState(results);
+
+  const handlePagination = async () => {
+    const tempPosts = [...posts];
+
+    const response = await fetch(next_page);
+    const data = await response.json();
+    const formattedResults = data.results.map(result => ({
+      ...result,
+      first_publication_date: format(
+        new Date(result.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    }));
+
+    const newPosts = [...tempPosts, ...formattedResults];
+    setNextPage(data.next_page);
+    setPosts(newPosts);
+  };
+
   return (
     <main className={styles.container}>
       <h1 className={styles.logo}>
         <img src="/images/logo.svg" alt="logo" />
       </h1>
       <ul className={styles.listContainer}>
-        <li className={styles.listContent}>
-          <a href="#">
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div>
-              <time>
-                <FiCalendar color="#BBBBBB" size="20px" /> 15 Mar 2021
-              </time>
-              <span>
-                <FiUser color="#BBBBBB" size="20px" /> Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </li>
-        <li className={styles.listContent}>
-          <a href="#">
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div>
-              <time>
-                <FiCalendar color="#BBBBBB" size="20px" /> 15 Mar 2021
-              </time>
-              <span>
-                <FiUser color="#BBBBBB" size="20px" /> Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </li>
+        {posts?.map(post => (
+          <li key={post.uid} className={styles.listContent}>
+            <Link href={`post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time>
+                    <FiCalendar color="#BBBBBB" size="20px" />{' '}
+                    {post.first_publication_date}
+                  </time>
+                  <span>
+                    <FiUser color="#BBBBBB" size="20px" /> {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          </li>
+        ))}
       </ul>
-
-      <button type="button" className={styles.btnLoadMore}>
-        Carregar mais posts
-      </button>
+      {nextPage && (
+        <button
+          type="button"
+          className={styles.btnLoadMore}
+          onClick={handlePagination}
+        >
+          Carregar mais posts
+        </button>
+      )}
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.Predicates.at('document.type', 'posts'),
+    { pageSize: 1 }
+  );
 
-//   // TODO
-// };
+  const formattedResults = postsResponse.results.map(result => ({
+    ...result,
+    first_publication_date: format(
+      new Date(result.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+  }));
+
+  postsResponse.results = formattedResults;
+
+  return {
+    props: {
+      postsPagination: postsResponse,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
