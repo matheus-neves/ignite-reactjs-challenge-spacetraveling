6b48b56ef63e82a45baead734aae5a8deba51565
Matheus Neves <matheusneves1989@gmail.com>
2021-04-21 14:27:48 -0300

    feat: Added html and styles to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +25,49 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.logo}>
+        <img src="/images/logo.svg" alt="logo" />
+      </h1>
+      <ul className={styles.listContainer}>
+        <li className={styles.listContent}>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar color="#BBBBBB" size="20px" /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser color="#BBBBBB" size="20px" /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </li>
+        <li className={styles.listContent}>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar color="#BBBBBB" size="20px" /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser color="#BBBBBB" size="20px" /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </li>
+      </ul>
+
+      <button type="button" className={styles.btnLoadMore}>
+        Carregar mais posts
+      </button>
+    </main>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
