d0b3828669a7a9731ace67919ac44f0c67c3e700
Matheus Neves <matheusneves1989@gmail.com>
2021-04-26 23:44:08 -0300

    feat: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={commonStyles.container}>
+      <Link href="/">
+        <a>
+          <img className={styles.logo} src="/images/logo.svg" alt="logo" />{' '}
+        </a>
+      </Link>
+    </header>
+  );
 }
